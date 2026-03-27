c25a081381443b6712e19f23192b14e92e468639
axtk <axtkx@inbox.ru>
2026-03-27 17:33:01 +0300

    replace regexps with string operations in join

--- src/utils/join.ts
+++ src/utils/join.ts
@@ -1,3 +1,9 @@
+function stripSlashes(s: string) {
+  while (s.endsWith("/")) s = s.slice(0, -1);
+  while (s.startsWith("/")) s = s.slice(1);
+  return s;
+}
+
 export function join(...args: string[]) {
   if (args.length === 0) return "";
 
@@ -7,16 +13,20 @@ export function join(...args: string[]) {
   let trailingPart = nonEmptyArgs.at(-1);
   let hasTrailingSlash = trailingPart?.endsWith("/") && trailingPart !== "/";
 
-  let result = nonEmptyArgs
-    .map((s) => s.replace(/^\/+|\/+$/g, ""))
-    .filter(Boolean)
-    .join("/");
+  let result = nonEmptyArgs.map(stripSlashes).filter(Boolean).join("/");
 
   if (hasLeadingSlash) result = `/${result}`;
   if (hasTrailingSlash) result = `${result}/`;
 
-  let head = result.match(/^(\w+:)?\/\//)?.[0] ?? "";
+  let head = "";
+
+  if (result.startsWith("//")) head = "//";
+  else if (result.includes("://"))
+    head = result.slice(0, result.indexOf("://") + 3);
+
   let tail = result.slice(head.length);
+
+  while (tail.includes("//")) tail = tail.replaceAll("//", "/");
 
   return `${head}${tail.replace(/\/+/g, "/")}`;
 }
